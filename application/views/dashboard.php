a8e5d16ccccdb1e284e37c3863f96c0854c1bc17
Rizal Nur Alfi <nuralfi.rizal@gmail.com>
2022-07-18 21:59:12 +0700

    percobaan dengan farid

---
+++ application/views/dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+	<link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+</head>
+<body>
+	<h2>Dashboard</h2>
+	<div class="container">
+		<div class="mb-3">
+			<label for="exampleFormControlInput1" class="form-label">Email address</label>
+			<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+		</div>
+		<div class="mb-3">
+			<label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+			<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+		</div>
+	</div>
+
+	<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+</body>
+</html>
