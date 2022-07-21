2731274e50cb7ffac454a23556af41e8654aab1d
nuralfir <nuralfi.rizal@gmail.com>
2022-07-21 14:29:54 +0700

    UI login dan dashboard

--- application/views/login.php
+++ application/views/login.php
@@ -5,25 +5,67 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
-	<link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
 </head>
 <body>
-	<h2>Login</h2>
 	<div class="container">
-		<form action="<?php echo base_url('Dashboard') ?>">
-			<div class="mb-3">
-				<label for="exampleFormControlInput1" class="form-label">Email address</label>
-				<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+		<!-- Outer Row -->
+		<div class="row justify-content-center">
+
+			<div class="col-xl-10 col-lg-12 col-md-9">
+
+				<div class="card o-hidden border-0 shadow-lg my-5">
+					<div class="card-body p-0">
+						<!-- Nested Row within Card Body -->
+						<div class="row">
+							<div class="col-lg-6">
+														
+							</div>
+							<div class="col-lg-6" style="">
+								<div class="p-5">
+									<div class="text-center">
+										<h1 class="h4 text-gray-900 mb-4">Welcome!</h1>
+									</div>
+									<form class="user">
+										<div class="form-group">
+											<input type="email" class="form-control form-control-user"
+												id="exampleInputEmail" aria-describedby="emailHelp"
+												placeholder="Enter Email Address...">
+										</div>
+										<div class="form-group">
+											<input type="password" class="form-control form-control-user"
+												id="exampleInputPassword" placeholder="Password">
+										</div>
+										<div class="form-group">
+											<div class="custom-control custom-checkbox small">
+												<input type="checkbox" class="custom-control-input" id="customCheck">
+												<label class="custom-control-label" for="customCheck">Remember
+													Me</label>
+											</div>
+										</div>
+										<a href="<?php echo base_url('Dashboard')?>" class="btn btn-primary btn-user btn-block">
+											Login
+										</a>										
+									</form>
+									<hr>
+									<div class="text-center">
+										<a class="small" href="forgot-password.html">Forgot Password?</a>
+									</div>
+									<div class="text-center">
+										<a class="small" href="register.html">Create an Account!</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
-			<div class="mb-3">
-				<label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-				<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-			</div>
-			<button type="submit" class="btn btn-primary">Primary</button>
-		</form>
-		<a href="<?php echo base_url('dashboard2') ?>" class="btn btn-primary">Login</a>
+
+		</div>`
 	</div>
 
-	<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+	<!---------- Javascript ---------->
+	<script src="<?php echo base_url('assets/js/sb-admin-2.min.js') ?>"></script>
 </body>
 </html>
