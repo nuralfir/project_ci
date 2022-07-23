6f1f1f9710e5f1060649a7ff583c186a3140ba93
nuralfir <nuralfi.rizal@gmail.com>
2022-07-23 16:23:40 +0700

    Update title

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -18,7 +18,7 @@
 				<div class="sidebar-brand-icon rotate-n-15">
 					<i class="fas fa-laugh-wink"></i>
 				</div>
-				<div class="sidebar-brand-text mx-3">Gudangque</div>
+				<div class="sidebar-brand-text mx-3">Gudangque Besar</div>
 			</a>
 
 			<!-- Divider -->
